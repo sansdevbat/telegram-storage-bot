1886131070ea15bdd43b1c1787ce0bc810646390
sansdevbat <sans.dev.bat@gmail.com>
2026-02-22 23:45:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 # Bot Configuration
-BOT_TOKEN = os.environ.get('BOT_TOKEN', 'YOUR_BOT_TOKEN_HERE')
-GROUP_ID = os.environ.get('GROUP_ID', '-1001234567890')  # Your group ID
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '8034509631:AAGVRCEUdZ5JkTjX_XITf8PbSfxc4BWncY8')
+GROUP_ID = os.environ.get('GROUP_ID', '-1002867274735')  # Your group ID
 
 # Database
 DATABASE_NAME = 'storage.db'
@@ -11,8 +11,8 @@ DATABASE_NAME = 'storage.db'
 ADMIN_IDS = [123456789, 987654321]  # Apne Telegram IDs daalein
 
 # Channel/Group Links
-GROUP_LINK = "https://t.me/your_group"  # Apna group link
-CHANNEL_LINK = "https://t.me/your_channel"  # Agar channel bhi hai to
+GROUP_LINK = "https://t.me/+sI3BK2GCnZRkM2Nl"  # Apna group link
+CHANNEL_LINK = "https://t.me/sarkariupdatex"  # Agar channel bhi hai to
 
 # File size limit (in MB)
 MAX_FILE_SIZE = 2000  # 2GB limit
